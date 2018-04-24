324e98a1b9520e19366fbb7b62a1990268f8d89c
Chris Hamons <chris.hamons@gmail.com>
2018-04-23 23:15:46 -0500

    Draw castbars. Refactor style out

--- src/ArenaLS.Views/Views/Combat/CharacterRenderer.cs
+++ src/ArenaLS.Views/Views/Combat/CharacterRenderer.cs
@@ -2,53 +2,52 @@
 
 namespace ArenaLS.Views.Views.Combat
 {
-	class CharacterRenderer
+	enum CharacterStyle { Normal, Double, ExtraLarge }
+
+	struct CharacterStyleInfo
 	{
-		enum CharacterStyle { Normal, Double, ExtraLarge }
-		const int BackgroundOffsetX = -5;
-		const int BackgroundOffsetY = -15;
-		static CharacterStyleInfo [] Styles = new CharacterStyleInfo [] {
-			new CharacterStyleInfo (26, 36, 52, 72, 60, 30, 60 + BackgroundOffsetX, 30 + BackgroundOffsetY),
-			new CharacterStyleInfo (52, 72, 52, 72, 60, 30, 60 + BackgroundOffsetX, 30 + BackgroundOffsetY),
-			new CharacterStyleInfo (122, 114, 122, 114, 21, 140, 21 + BackgroundOffsetX, 140 + BackgroundOffsetY),
+		public static CharacterStyleInfo [] Styles = new CharacterStyleInfo [] {
+			new CharacterStyleInfo (26, 36, 52, 72, 60, 30, -55, 16),
+			new CharacterStyleInfo (52, 72, 52, 72, 60, 30, -55, 16),
+			new CharacterStyleInfo (122, 114, 122, 114, 21, 152, 29, 127),
 		};
 
-		struct CharacterStyleInfo
+		public readonly int Width;
+		public readonly int Height;
+		public readonly int RenderWidth;
+		public readonly int RenderHeight;
+		public readonly int TextXOffset;
+		public readonly int TextYOffset;
+		public readonly int CastXOffset;
+		public readonly int CastYOffset;
+
+		public CharacterStyleInfo (int width, int height, int renderWidth, int renderHeight, int textXOffset, int textYOffset, int castXOffset, int castYOffset)
 		{
-			public readonly int Width;
-			public readonly int Height;
-			public readonly int RenderWidth;
-			public readonly int RenderHeight;
-			public readonly int TextXOffset;
-			public readonly int TextYOffset;
-			public readonly int BackgroundXOffset;
-			public readonly int BackgroundYOffset;
-
-			public CharacterStyleInfo (int width, int height, int renderWidth, int renderHeight, int textXOffset, int textYOffset, int backgroundXOffset, int backgroundYOffset)
-			{
-				Width = width;
-				Height = height;
-				RenderWidth = renderWidth;
-				RenderHeight = renderHeight;
-				TextXOffset = textXOffset;
-				TextYOffset = textYOffset;
-				BackgroundXOffset = backgroundXOffset;
-				BackgroundYOffset = backgroundYOffset;
-			}
+			Width = width;
+			Height = height;
+			RenderWidth = renderWidth;
+			RenderHeight = renderHeight;
+			TextXOffset = textXOffset;
+			TextYOffset = textYOffset;
+			CastXOffset = castXOffset;
+			CastYOffset = castYOffset;
 		}
+	}
 
+	class CharacterRenderer
+	{
 		TilesetLoader CharacterLoader;
-		TilesetLoader StatusIconLoader;
+		HUDRenderer HUDRenderer;
 		int StartingID;
 		CharacterStyleInfo StyleInfo;
 
 		CharacterRenderer (TilesetLoader characterLoader, int startingID, CharacterStyleInfo styleInfo)
 		{
 			CharacterLoader = characterLoader;
-			StatusIconLoader = new TilesetLoader ("data/tf_icon_32.png", 32);
-
 			StartingID = startingID;
 			StyleInfo = styleInfo;
+
+			HUDRenderer = new HUDRenderer (StyleInfo);
 		}
 
 		static CharacterRenderer Create (string path, int startingID, CharacterStyleInfo style)
@@ -59,45 +58,33 @@ namespace ArenaLS.Views.Views.Combat
 
 		public static CharacterRenderer CreateNormalSized (string path, int startingID, bool doubleSize)
 		{
-			var styleInfo = doubleSize ? Styles [(int)CharacterStyle.Double] : Styles [(int)CharacterStyle.Normal];
+			var styleInfo = doubleSize ? CharacterStyleInfo.Styles [(int)CharacterStyle.Double] : CharacterStyleInfo.Styles [(int)CharacterStyle.Normal];
 			return Create (path, startingID, styleInfo);
 		}
 
 		public static CharacterRenderer CreateExtraLarge (string path, int startingID)
 		{
-			return Create (path, startingID, Styles [(int)CharacterStyle.ExtraLarge]);
+			return Create (path, startingID, CharacterStyleInfo.Styles [(int)CharacterStyle.ExtraLarge]);
 		}
 
 		readonly int [] FrameOffset = new int [] { 1, 0, 1, 2 };
 		int CalculateAnimationOffset (long frame)
 		{
 			const int FramesBetweenAnimation = 6;
-			return FrameOffset [(int)((frame / FramesBetweenAnimation) % 4)];		
+			return FrameOffset [(int)((frame / FramesBetweenAnimation) % 4)];
 		}
-
-		readonly SKPaint TextPaint = new SKPaint () {
-			TextSize = 14, Color = new SKColor (238, 238, 238),
-			IsAntialias = true, IsAutohinted = true, StrokeWidth = 3
-		};
-
-		readonly SKPaint TextBackground = new SKPaint () { Color = SKColors.Gray.WithAlpha (160) };
-		readonly SKPaint AntialiasPaint = new SKPaint () { IsAntialias = false };
 
 		public void Render (SKCanvas canvas, int x, int y, long frame)
 		{
+			DrawCharacter (canvas, x, y, frame);
+			HUDRenderer.DrawHUD (canvas, x, y);
+			HUDRenderer.DrawCastbar (canvas, x, y, frame);
+		}
+
+		void DrawCharacter (SKCanvas canvas, int x, int y, long frame)
+		{
 			var tilesetRect = CharacterLoader.GetRect (StartingID + CalculateAnimationOffset (frame));
 			canvas.DrawBitmap (CharacterLoader.Tileset, tilesetRect, SKRect.Create (x, y, StyleInfo.RenderWidth, StyleInfo.RenderHeight));
-			canvas.DrawRect (SKRect.Create (x + StyleInfo.BackgroundXOffset, y + StyleInfo.BackgroundYOffset, 85, 60), TextBackground);
-			canvas.DrawText ("NameName", new SKPoint (x + StyleInfo.TextXOffset, y + StyleInfo.TextYOffset), TextPaint);
-			canvas.DrawText ("HP 999/888", new SKPoint (x + StyleInfo.TextXOffset, 18 + y + StyleInfo.TextYOffset), TextPaint);
-			// TestData
-			int iconOffset = 0;
-			foreach (int i in new int [] { 33, 42, 148, 36 })
-			{
-				var bitmapRect = SKRect.Create (x + StyleInfo.TextXOffset + (iconOffset * 20), 24 + y + StyleInfo.TextYOffset, 16, 16);
-				canvas.DrawBitmap (StatusIconLoader.Tileset, StatusIconLoader.GetRect (i), bitmapRect, AntialiasPaint);
-				iconOffset++;
-			}
 		}
 	}
 }
---
+++ src/ArenaLS.Views/Views/Styles.cs
@@ -0,0 +1,29 @@
+﻿using SkiaSharp;
+
+namespace ArenaLS.Views.Views
+{
+	static class Styles
+	{
+		public static SKPaint TextPaint = new SKPaint ()
+		{
+			TextSize = 14,
+			Color = new SKColor (238, 238, 238),
+			IsAntialias = true,
+			IsAutohinted = true,
+			StrokeWidth = 3
+		};
+
+		public static SKPaint SmallTextPaint = new SKPaint ()
+		{
+			TextSize = 9,
+			Color = new SKColor (238, 238, 238),
+			IsAntialias = true,
+			IsAutohinted = true,
+			StrokeWidth = 2
+		};
+
+		public static readonly SKPaint TextBackground = new SKPaint () { Color = SKColors.Gray.WithAlpha (160) };
+		public static readonly SKPaint TextBackgroundDark = new SKPaint () { Color = SKColors.DarkGray.WithAlpha (160) };
+		public static readonly SKPaint AntialiasPaint = new SKPaint () { IsAntialias = false };
+	}
+}
